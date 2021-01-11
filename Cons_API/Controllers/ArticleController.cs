894dacd92f8283b2e73e5b8ba3578281cfd0f3f1
oussama Wiswis <oussamawiswis1991@gmail.com>
2021-01-11 14:43:28 +0100

    validation
    Éléments de travail associés : #4; #8; #10

---
+++ Cons_API/Controllers/ArticleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Cons_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cons_API.Controllers
+{
+    public class ArticleController : Controller
+    {
+       
+            public IActionResult Index()
+            {
+                IEnumerable<ArticleGenerique> listearticles;
+                HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("ArticleGenerique").Result;
+                listearticles = response.Content.ReadAsAsync<IEnumerable<ArticleGenerique>>().Result;
+
+                return View(listearticles);
+            
+        }
+    }
+}
---
+++ Cons_API/Controllers/ClientController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Cons_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cons_API.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            IEnumerable<Client> listeclients;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Client").Result;
+            listeclients = response.Content.ReadAsAsync<IEnumerable<Client>>().Result;
+
+            return View(listeclients);
+        }
+    }
+}
